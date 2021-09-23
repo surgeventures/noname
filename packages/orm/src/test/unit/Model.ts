54dcfe60c0ce9b4cd14ec54ed8c1a79f2a24ac8d
Rafal Panasiuk <rafal.panasiuk@fresha.com>
2021-09-23 19:03:52 +0200

    PRT-236 Typed Model tests

--- packages/orm/src/test/unit/Model.ts
+++ packages/orm/src/test/unit/Model.ts
@@ -1,82 +1,81 @@
-import { ORM, Model as BaseModel, QuerySet, attr, Session } from "../..";
+import { ORM, Model, QuerySet, attr } from "../..";
 import { castTo } from "../../hacks";
+import { ModelId, SessionLike } from "../../types";
+
 
 describe("Model", () => {
+  const getTestModelClass = () => {
+    return class Test extends Model<typeof Test, {}> {
+      static modelName = "UnitTestModel" as const;
+    };
+  };
+
   describe("static method", () => {
-    type SessionWithModel = Session & {
-      UnitTestModel: typeof BaseModel;
+    type Schema = {
+      UnitTestModel: ReturnType<typeof getTestModelClass>;
     };
 
-    let Model: typeof BaseModel;
-    let sessionMock: SessionWithModel;
+    let TestModel: ReturnType<typeof getTestModelClass>;
+    let sessionMock: SessionLike<Schema>;
 
     beforeEach(() => {
-      // Get a fresh copy
-      // of Model, so our manipulations
-      // won't survive longer than each test.
-      Model = class extends BaseModel {
-        static modelName = "UnitTestModel";
-      };
-
-      const orm = new ORM();
-      orm.register(Model);
-      sessionMock = castTo<SessionWithModel>(orm.session());
+      TestModel = getTestModelClass();
+      const orm = new ORM<Schema>();
+      orm.register(TestModel);
+      sessionMock = orm.session();
     });
 
     it("make sure instance methods are enumerable", () => {
-      // See #29.
-
-      const enumerableProps: Record<string, any> = {};
-      for (const propName in Model) {
-        // eslint-disable-line
-        enumerableProps[propName] = true;
+      const enumerableProps: Record<keyof typeof TestModel, boolean> = {} as Record<keyof typeof TestModel, boolean>;
+      for (const propName in TestModel) {
+        enumerableProps[propName as keyof typeof TestModel] = true;
       }
 
       expect(enumerableProps.modelName).toBe(true);
-      // expect(enumerableProps.create).toBe(true);
+      expect(enumerableProps.create).toBe(true);
     });
 
     it("session getter works correctly", () => {
-      expect(Model.session).toBeUndefined();
-      Model._session = sessionMock;
-      expect(Model.session).toBe(sessionMock);
+      expect(TestModel.session).toBeUndefined();
+      TestModel._session = sessionMock;
+      expect(TestModel.session).toBe<SessionLike<Schema>>(sessionMock);
     });
 
     it("connect defines session statically on Model", () => {
-      expect(Model.session).toBeUndefined();
-      Model.connect(sessionMock);
-      expect(Model.session).toBe(sessionMock);
+      expect(TestModel.session).toBeUndefined();
+      TestModel.connect(sessionMock);
+      expect(TestModel.session).toBe<SessionLike<Schema>>(sessionMock);
     });
 
-    // it("connect throws if not passing a session", () => {
-    //   expect(Model.session).toBeUndefined();
-    //   [1, "", [], {}].forEach((value) =>
-    //     expect(() => {
-    //       Model.connect(value);
-    //     }).toThrow("A model can only be connected to instances of Session.")
-    //   );
-    // });
+    it("connect throws if not passing a session", () => {
+      expect(Model.session).toBeUndefined();
+      [1, "", [], {}].forEach((value) =>
+        expect(() => {
+          Model.connect(value as any);
+        }).toThrow("A model can only be connected to instances of Session.")
+      );
+    });
 
     it("toString works correctly", () => {
-      expect(Model.toString()).toBe("ModelClass: UnitTestModel");
+      expect(TestModel.toString()).toBe("ModelClass: UnitTestModel");
     });
 
     it("query returns QuerySet", () => {
-      expect(Model.query).toBeInstanceOf(QuerySet);
+      expect(TestModel.query).toBeInstanceOf(QuerySet);
     });
 
     it("getQuerySet returns QuerySet", () => {
-      expect(Model.getQuerySet()).toBeInstanceOf(QuerySet);
+      expect(TestModel.getQuerySet()).toBeInstanceOf(QuerySet);
     });
 
     it("all returns QuerySet", () => {
-      expect(Model.all()).toBeInstanceOf(QuerySet);
+      expect(TestModel.all()).toBeInstanceOf(QuerySet);
     });
 
     it("markAccessed correctly proxies to Session", () => {
-      Model.connect(sessionMock);
-      Model.markAccessed([1, 3]);
-      expect(sessionMock.accessedModelInstances).toEqual({
+      TestModel.connect(sessionMock);
+      TestModel.markAccessed([1, 3]);
+      expect(sessionMock.accessedModelInstances).toEqual<Record<keyof Schema, Record<ModelId, boolean>>>({
         UnitTestModel: {
           1: true,
           3: true,
@@ -85,9 +84,9 @@ describe("Model", () => {
     });
 
     it("markFullTableScanned correctly proxies to Session", () => {
-      Model.connect(sessionMock);
-      Model.markFullTableScanned();
-      expect(sessionMock.fullTableScannedModels).toEqual(["UnitTestModel"]);
+      TestModel.connect(sessionMock);
+      TestModel.markFullTableScanned();
+      expect(sessionMock.fullTableScannedModels).toEqual<(keyof Schema)[]>(["UnitTestModel"]);
     });
 
     it("should throw a custom error when user try to interact with database without a session", () => {
@@ -97,49 +96,63 @@ describe("Model", () => {
         number: 123,
         boolean: false,
       };
-      expect(() => Model.create(attributes)).toThrow(
+      expect(() => TestModel.create(attributes)).toThrow(
         'Tried to create a UnitTestModel model instance without a session. Create a session using `session = orm.session()` and call `session["UnitTestModel"].create` instead.'
       );
-      expect(() => Model.upsert(attributes)).toThrow(
+      expect(() => TestModel.upsert(attributes)).toThrow(
         'Tried to upsert a UnitTestModel model instance without a session. Create a session using `session = orm.session()` and call `session["UnitTestModel"].upsert` instead.'
       );
-      expect(() => Model.exists(attributes)).toThrow(
+      expect(() => TestModel.exists(attributes)).toThrow(
         'Tried to check if a UnitTestModel model instance exists without a session. Create a session using `session = orm.session()` and call `session["UnitTestModel"].exists` instead.'
       );
-      expect(() => Model.withId(0)).toThrow(
+      expect(() => TestModel.withId(0)).toThrow(
         'Tried to get the UnitTestModel model\'s id attribute without a session. Create a session using `session = orm.session()` and access `session["UnitTestModel"].idAttribute` instead.'
       );
-      expect(() => new Model().update(attributes)).toThrow(
+      expect(() => new TestModel({}).update(attributes)).toThrow(
         "Tried to update a UnitTestModel model instance without a session. You cannot call `.update` on an instance that you did not receive from the database."
       );
-      expect(() => new Model().delete()).toThrow(
+      expect(() => new TestModel({}).delete()).toThrow(
         "Tried to delete a UnitTestModel model instance without a session. You cannot call `.delete` on an instance that you did not receive from the database."
       );
     });
   });
 
   describe("Instance methods", () => {
-    class Model extends BaseModel {
-      static modelName = "UnitTestModel";
-      static fields = {
-        id: attr(),
-        name: attr(),
-        number: attr(),
-        boolean: attr(),
-        array: attr(),
-        object: attr(),
-      };
+    type TestDescriptors = {
+      id: ModelId;
+      name: string;
+      number: number;
+      boolean: boolean;
+      array: any[];
+      object: {};
     }
+    type Schema = {
+      UnitTestModel: ReturnType<typeof getTestModelClass>; 
+    }
+    const getTestModelClass = () => {
+      return class Test extends Model<typeof Test, TestDescriptors> {
+        static modelName = "UnitTestModel" as const;
+        static fields = {
+          id: attr(),
+          name: attr(),
+          number: attr(),
+          boolean: attr(),
+          array: attr(),
+          object: attr(),
+        };
+      };
+    };
+
+    let TestModel: ReturnType<typeof getTestModelClass>;
 
     beforeEach(() => {
-      const XModel = class extends Model {};
-
-      const orm = new ORM();
-      orm.register(XModel);
+      TestModel = getTestModelClass();
+      const orm = new ORM<Schema>();
+      orm.register(TestModel);
     });
 
     it("getClass works correctly", () => {
-      const instance = new Model({
+      const instance = new TestModel({
         id: 0,
         name: "Tommi",
         array: [],
@@ -147,24 +160,24 @@ describe("Model", () => {
         number: 123,
         boolean: false,
       });
-      expect(instance.getClass()).toBe(Model);
+      expect(instance.getClass()).toBe(TestModel);
     });
 
     it("equals compares primitive types correctly", () => {
-      const instance1 = new Model({
+      const instance1 = new TestModel({
         id: 0,
         name: "Tommi",
         number: 123,
         boolean: true,
       });
-      const instance2 = new Model({
+      const instance2 = new TestModel({
         id: 0,
         name: "Tommi",
         number: 123,
         boolean: true,
       });
       expect(instance1.equals(instance2)).toBeTruthy();
-      const instance3 = new Model({
+      const instance3 = new TestModel({
         id: 0,
         name: "Tommi",
         number: 123,
@@ -174,23 +187,23 @@ describe("Model", () => {
     });
 
     it("equals does not deeply compare array fields", () => {
-      const instance1 = new Model({ id: 0, array: [] });
-      const instance2 = new Model({ id: 0, array: [] });
+      const instance1 = new TestModel({ id: 0, array: [] });
+      const instance2 = new TestModel({ id: 0, array: [] });
       expect(instance1.equals(instance2)).toBeFalsy();
     });
 
     it("equals does not deeply compare object fields", () => {
-      const instance1 = new Model({ id: 0, object: {} });
-      const instance2 = new Model({ id: 0, object: {} });
+      const instance1 = new TestModel({ id: 0, object: {} });
+      const instance2 = new TestModel({ id: 0, object: {} });
       expect(instance1.equals(instance2)).toBeFalsy();
     });
 
     it("constructing with random attributes assigns these attributes", () => {
       const randomNumber = Math.random();
-      const model = new Model({
+      const model = new TestModel({
         randomNumber,
         someString: "some string",
-      });
+      } as any);
       expect(castTo<{ randomNumber: string }>(model).randomNumber).toBe(
         randomNumber
       );
